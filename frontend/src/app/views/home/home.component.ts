a6a36e065b9f3041e8dd0e775041df8dc0335946
lucasmp1945 <pradolucas910@gmail.com>
2024-07-23 18:46:44 -0300

    seg

---
+++ frontend/src/app/views/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent {
+
+  constructor(private apiService: ApiService) { }
+
+
+  ngOnInit(): void {
+    this.apiService.getHello().subscribe(response => {
+      console.log(response);
+    });
+  }
+
+}
